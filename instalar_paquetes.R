41734b110b2ad8baa2b2cfd8a9294d2591c2c549
Nick <nnapolitano88@gmail.com>
2024-12-20 19:17:48 -0300

    Fixed spelling error in ISLR2 package

--- instalar_paquetes.R
+++ instalar_paquetes.R
@@ -1,7 +1,7 @@
 # List of packages to install
 packages <- c("class", "e1071", "caret", "rpart", "nnet", "stats", 
               "cluster", "ggplot2", "MASS", "dplyr", "tidyr", 
-              "readr", "plotly", "psych, ILSPR2")
+              "readr", "plotly", "psych, ISLR2")
 
 # Install packages if not already installed
 installed_packages <- installed.packages()[, "Package"]
@@ -10,4 +10,3 @@ packages_to_install <- setdiff(packages, installed_packages)
 if (length(packages_to_install) > 0) {
   install.packages(packages_to_install, dependencies = TRUE)
 }
- 
